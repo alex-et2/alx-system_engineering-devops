e047b4764dd7ebfc94e287516329d85d6501802b
alex-et2 <118194790+alex-et2@users.noreply.github.com>
2023-10-28 02:09:12 -0700

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# puppet manifest creates a file
-file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# Puppet manifest to install 'puppet-lint'
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
-}
